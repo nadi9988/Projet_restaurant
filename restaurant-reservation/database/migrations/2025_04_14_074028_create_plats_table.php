8b5ef7283d3c97bd633b6a3d05bd91e0daffc25b
Mohamed-Amine-Manaoui <mohamedamine.manaoui@gmail.com.com>
2025-04-14 08:46:55 +0100

    create plats_table

---
+++ restaurant-reservation/database/migrations/2025_04_14_074028_create_plats_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('plats', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('menu_categorie_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->decimal('price', 8, 2);
+            $table->string('image')->nullable();
+            $table->boolean('available')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('plats');
+    }
+};
