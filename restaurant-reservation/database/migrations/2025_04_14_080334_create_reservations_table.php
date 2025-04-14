b152220f0441594d2c883cbb44d40b55386e0095
Mohamed-Amine-Manaoui <mohamedamine.manaoui@gmail.com.com>
2025-04-14 09:09:44 +0100

    create reservationstable

---
+++ restaurant-reservation/database/migrations/2025_04_14_080334_create_reservations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('client_id')->constrained()->onDelete('cascade');
+            $table->foreignId('table_id')->constrained()->onDelete('cascade');
+            $table->dateTime('date_time');
+            $table->integer('number_of_people');
+            $table->string('status')->default('en_attente');
+            $table->text('comments')->nullable();
+            $table->text('order_pre_dishes')->nullable(); // ila bgha yakl xi 7aja khfifa 9bl maywjd lplat dialo !
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
