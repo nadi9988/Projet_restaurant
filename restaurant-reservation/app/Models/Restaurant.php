428327e9684400ad6103cef45337987912741639
Mohamed-Amine-Manaoui <mohamedamine.manaoui@gmail.com.com>
2025-04-17 02:47:43 +0100

    create Restaurant Model without Method

---
+++ restaurant-reservation/app/Models/Restaurant.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Restaurant extends Model
+{
+    protected $fillable = [
+        'nom',
+        'adresse',
+        'telephone',
+        'email',
+        'description',
+    ];
+
+    public function horaire()
+    {
+        return $this->hasOne(Horaire::class);
+    }
+
+    public function tables()
+    {
+        return $this->hasMany(Table::class);
+    }
+
+    public function menuCategories()
+    {
+        return $this->hasMany(MenuCategorie::class);
+    }
+    //Method Soon . . .
+}
