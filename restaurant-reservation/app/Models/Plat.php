c3137898d03a9c17d4bb9afdd1a49885c64be50f
Mohamed-Amine-Manaoui <mohamedamine.manaoui@gmail.com.com>
2025-04-18 02:14:25 +0100

    create Plat and Ingredient Model without Method

--- restaurant-reservation/app/Models/Plat.php
+++ restaurant-reservation/app/Models/Plat.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Plat extends Model
 {
-    //
+    protected $fillable = [
+        'menu_categorie_id',
+        'nom',
+        'description',
+        'price',
+        'image',
+        'available',
+    ];
+
+    protected $casts = [
+        'price' => 'float',
+        'available' => 'boolean',
+    ];
+
+    public function menuCategorie()
+    {
+        return $this->belongsTo(MenuCategorie::class);
+    }
+
+    public function ligneCommandes()
+    {
+        return $this->hasMany(LigneCommande::class);
+    }
+    // Method Soon . . . 
 }
