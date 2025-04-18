f13f6030930169c99e864bc1b816013c3a7990d5
Mohamed-Amine-Manaoui <mohamedamine.manaoui@gmail.com.com>
2025-04-18 02:02:37 +0100

    create Table Model without Method

--- restaurant-reservation/app/Models/Table.php
+++ restaurant-reservation/app/Models/Table.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Table extends Model
 {
-    //
+    protected $fillable = [
+        'restaurant_id',
+        'numero',
+        'capacity',
+        'available',
+        'location',
+    ];
+
+    protected $casts = [
+        'available' => 'boolean',
+    ];
+
+    public function restaurant()
+    {
+        return $this->belongsTo(Restaurant::class);
+    }
+
+    public function reservations()
+    {
+        return $this->hasMany(Reservation::class);
+    }
+    //Method Soon . . .
 }
