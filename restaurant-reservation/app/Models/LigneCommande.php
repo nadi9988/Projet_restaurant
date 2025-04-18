f2acf2c1e2e97cf2a0451213cca6cd76663d8fd1
Mohamed-Amine-Manaoui <mohamedamine.manaoui@gmail.com.com>
2025-04-18 02:36:04 +0100

    create LingneCommande Model without Method

--- restaurant-reservation/app/Models/LigneCommande.php
+++ restaurant-reservation/app/Models/LigneCommande.php
@@ -6,6 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class LigneCommande extends Model
 {
+    protected $fillable = [
+        'commande_id',
+        'plat_id',
+        'quantity',
+        'unit_price',
+        'instructions',
+        ];
+        protected $casts = [
+        'quantity' => 'integer',
+        'unit_price' => 'float',
+    ];
 
+    public function commande()
+    {
+        return $this->belongsTo(Commande::class);
+    }
+    public function plat()
+    {
+        return $this->belongsTo(Plat::class);
+    }
     // Method Soon . . . 
 }
