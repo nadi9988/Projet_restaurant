41ae4a46ca014ec292f2a6976291af94f5a64d4e
Mohamed-Amine-Manaoui <mohamedamine.manaoui@gmail.com.com>
2025-04-18 02:09:15 +0100

    create MenuCategorie Model without Method

--- restaurant-reservation/app/Models/MenuCategorie.php
+++ restaurant-reservation/app/Models/MenuCategorie.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class MenuCategorie extends Model
 {
-    //
+    protected $fillable = [
+        'restaurant_id',
+        'name',
+        'description',
+    ];
+
+    public function restaurant()
+    {
+        return $this->belongsTo(Restaurant::class);
+    }
+
+    public function plats()
+    {
+        return $this->hasMany(Plat::class);
+    }
+    // Method Soon . . . 
 }
