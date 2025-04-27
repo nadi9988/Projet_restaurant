4b98bf9e58d5fcb882580aa237af63891ad6cc9b
Yassir kniksi <kniksi.yassir@gmail.com>
2025-04-27 19:15:41 +0100

    test

--- restaurant-reservation/routes/web.php
+++ restaurant-reservation/routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+
+
+
 Route::get('/welcome', function () {
     return view('welcome');
 });
@@ -27,6 +31,3 @@ Route::get('/Reservation', function () {
 Route::get('/Admin', function () {
     return view ('Admin');
 });
-
-
-
